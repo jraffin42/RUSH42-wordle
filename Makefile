00b470509b37fc50b1963f5b4368f1dd4838efe4
cdittric <cdittric@student.42.fr>
2022-05-14 18:17:45 +0200

    Link with SDL_ttf and hello world window

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC =       clang++
 CXX =      clang++
 CXXFLAGS = -Wall -Wextra $(shell $(SDL2_CONFIG) --cflags)
-LDLIBS =   $(shell $(SDL2_CONFIG) --static-libs)
+LDLIBS =   $(shell $(SDL2_CONFIG) --static-libs) -lSDL2_ttf
 
 NAME = wordle
 SRC =  wordle.cpp
@@ -9,31 +9,44 @@ OBJ =  $(SRC:.cpp=.o)
 
 DIR := $(shell pwd)
 SDL2 =        SDL2-2.0.22
+SDL2_TTF =    SDL2_ttf-2.0.18
 SDL2_CONFIG = local/bin/sdl2-config
+SDL2_TTF_A =  local/lib/libSDL2_ttf.la
 
 all: $(NAME)
 $(NAME): $(OBJ)
-$(OBJ): $(SDL2_CONFIG)
+$(OBJ): $(SDL2_CONFIG) $(SDL2_TTF_A)
 clean:
 	rm -vf $(OBJ)
-fclean:
-	@rm -vf $(OBJ)
+fclean: clean
 	@rm -vf $(NAME)
 	rm -rf local
 	rm -rf $(SDL2)
+	rm -rf $(SDL2_TTF)
 re: fclean all
 reme: clean all
-.PHONY: all clean fclean
+.PHONY: all clean fclean SDL2
 
-$(SDL2):
+$(SDL2_CONFIG):
 	@printf "Downloading SDL2...\n"
 	@curl -fsSL https://libsdl.org/release/SDL2-2.0.22.tar.gz | tar -xz
-$(SDL2_CONFIG): $(SDL2)
 	@printf "Configuring SDL2...\n"
 	@mkdir -p $(SDL2)/build && \
 	     cd $(SDL2)/build && \
-	     ../configure --prefix=$(DIR)/local --disable-shared #>/dev/null
+	     ../configure --prefix=$(DIR)/local --disable-shared --disable-dependency-tracking #>/dev/null
 	@printf "Building SDL2...\n"
 	@cd $(SDL2)/build && make -j4
 	@printf "Installing SDL2 locally...\n"
 	@cd $(SDL2)/build && make install >/dev/null
+
+$(SDL2_TTF_A):
+	@printf "Downloading SDL2_ttf...\n"
+	@curl -fsSL https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.0.18/SDL2_ttf-2.0.18.tar.gz | tar -xz
+	@printf "Configuring SDL2_ttf...\n"
+	@mkdir -p $(SDL2_TTF)/build && \
+	     cd $(SDL2_TTF)/build && \
+	     ../configure --prefix=$(DIR)/local --disable-shared --disable-dependency-tracking #>/dev/null
+	@printf "Building SDL2_ttf...\n"
+	@cd $(SDL2_TTF)/build && make -j4
+	@printf "Installing SDL2_ttf locally...\n"
+	@cd $(SDL2_TTF)/build && make install >/dev/null
--- wordle.cpp
+++ wordle.cpp
@@ -1,6 +1,147 @@
 #include <iostream>
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_ttf.h>
+
+void print_event_type(Uint32 event);
+
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+static void cleanup() {
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+}
+
+static void quit(int status) {
+	exit (status);
+}
+
+#define sdlfail() sdlfail_(__FILE__, __LINE__)
+#define ttffail() ttffail_(__FILE__, __LINE__)
+#define safeatexit(func) safeatexit_(__FILE__, __LINE__, func)
+static void sdlfail_(const char *file, int line) {
+	fprintf(stderr, "%s:%d: %s\n", file, line, SDL_GetError());
+	exit(1);
+}
+static void ttffail_(const char *file, int line) {
+	fprintf(stderr, "%s:%d: %s\n", file, line, TTF_GetError());
+	exit(1);
+}
+static void safeatexit_(const char *file, int line, void (*func)(void)) {
+	if (atexit(func) != 0) {
+		fprintf(stderr, "%s:%d: atexit could not register function\n", file, line);
+		exit(1);
+	}
+}
 
 int main() {
-	std::cout << "Hello, World!\n";
-	return 0;
+	SDL_Event event;
+	int ret;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) sdlfail();
+	safeatexit(SDL_Quit);
+	if (TTF_Init() < 0) ttffail();
+	safeatexit(TTF_Quit);
+
+	safeatexit(cleanup);
+	window = SDL_CreateWindow("Wordle",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		640, 480,
+		SDL_WINDOW_RESIZABLE);
+	if (window == NULL) sdlfail();
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (renderer == NULL) sdlfail();
+
+	while ((ret = SDL_WaitEvent(&event)) && event.type != SDL_QUIT) {
+		switch (event.type) {
+
+		case SDL_WINDOWEVENT:
+			switch (event.window.event) {
+			case SDL_WINDOWEVENT_EXPOSED:
+				ret = SDL_RenderClear(renderer);
+				if (ret < 0) sdlfail();
+				SDL_RenderPresent(renderer);
+				break;
+			default: print_event_type(event.type);
+			}
+			break;
+		
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym) {
+			case SDLK_ESCAPE:
+				exit(0);
+				break;
+			default: break;
+			}
+
+		default: print_event_type(event.type);
+		}
+	}
+	if (!ret) { fprintf(stderr, "wordle: %s\n", SDL_GetError()); quit(1); }
+	exit(0);
+}
+
+void print_event_type(Uint32 event) {
+	switch (event) {
+		case 0: printf("SDL_FIRSTEVENT\n"); 					break;
+		case 0x100: printf("SDL_QUIT\n"); 						break;
+		case 0x101: printf("SDL_APP_TERMINATING\n"); 			break;
+		case 0x102: printf("SDL_APP_LOWMEMORY\n"); 				break;
+		case 0x103: printf("SDL_APP_WILLENTERBACKGROUND\n"); 	break;
+		case 0x104: printf("SDL_APP_DIDENTERBACKGROUND\n"); 	break;
+		case 0x105: printf("SDL_APP_WILLENTERFOREGROUND\n"); 	break;
+		case 0x106: printf("SDL_APP_DIDENTERFOREGROUND\n"); 	break;
+		case 0x107: printf("SDL_LOCALECHANGED\n"); 				break;
+		case 0x150: printf("SDL_DISPLAYEVENT\n"); 				break;
+		case 0x200: printf("SDL_WINDOWEVENT\n"); 				break;
+		case 0x201: printf("SDL_SYSWMEVENT\n"); 				break;
+		case 0x300: printf("SDL_KEYDOWN\n"); 					break;
+		case 0x301: printf("SDL_KEYUP\n"); 						break;
+		case 0x302: printf("SDL_TEXTEDITING\n"); 				break;
+		case 0x303: printf("SDL_TEXTINPUT\n"); 					break;
+		case 0x304: printf("SDL_KEYMAPCHANGED\n"); 				break;
+		case 0x305: printf("SDL_TEXTEDITING_EXT,\n"); 			break;
+		case 0x400: printf("SDL_MOUSEMOTION\n"); 				break;
+		case 0x401: printf("SDL_MOUSEBUTTONDOWN\n"); 			break;
+		case 0x402: printf("SDL_MOUSEBUTTONUP\n"); 				break;
+		case 0x403: printf("SDL_MOUSEWHEEL\n"); 				break;
+		case 0x600: printf("SDL_JOYAXISMOTION\n"); 				break;
+		case 0x601: printf("SDL_JOYBALLMOTION\n"); 				break;
+		case 0x602: printf("SDL_JOYHATMOTION\n"); 				break;
+		case 0x603: printf("SDL_JOYBUTTONDOWN\n"); 				break;
+		case 0x604: printf("SDL_JOYBUTTONUP\n"); 				break;
+		case 0x605: printf("SDL_JOYDEVICEADDED\n"); 			break;
+		case 0x606: printf("SDL_JOYDEVICEREMOVED\n"); 			break;
+		case 0x650: printf("SDL_CONTROLLERAXISMOTION\n"); 		break;
+		case 0x651: printf("SDL_CONTROLLERBUTTONDOWN\n"); 		break;
+		case 0x652: printf("SDL_CONTROLLERBUTTONUP\n"); 		break;
+		case 0x653: printf("SDL_CONTROLLERDEVICEADDED\n"); 		break;
+		case 0x654: printf("SDL_CONTROLLERDEVICEREMOVED\n"); 	break;
+		case 0x655: printf("SDL_CONTROLLERDEVICEREMAPPED\n"); 	break;
+		case 0x656: printf("SDL_CONTROLLERTOUCHPADDOWN\n"); 	break;
+		case 0x657: printf("SDL_CONTROLLERTOUCHPADMOTION\n"); 	break;
+		case 0x658: printf("SDL_CONTROLLERTOUCHPADUP\n"); 		break;
+		case 0x659: printf("SDL_CONTROLLERSENSORUPDATE\n"); 	break;
+		case 0x700: printf("SDL_FINGERDOWN\n"); 				break;
+		case 0x701: printf("SDL_FINGERUP\n"); 					break;
+		case 0x702: printf("SDL_FINGERMOTION\n"); 				break;
+		case 0x800: printf("SDL_DOLLARGESTURE\n"); 				break;
+		case 0x801: printf("SDL_DOLLARRECORD\n"); 				break;
+		case 0x802: printf("SDL_MULTIGESTURE\n"); 				break;
+		case 0x900: printf("SDL_CLIPBOARDUPDATE\n"); 			break;
+		case 0x1000: printf("SDL_DROPFILE\n"); 					break;
+		case 0x1001: printf("SDL_DROPTEXT\n"); 					break;
+		case 0x1002: printf("SDL_DROPBEGIN,\n"); 				break;
+		case 0x1003: printf("SDL_DROPCOMPLETE,\n"); 			break;
+		case 0x1100: printf("SDL_AUDIODEVICEADDED\n"); 			break;
+		case 0x1101: printf("SDL_AUDIODEVICEREMOVED\n"); 		break;
+		case 0x1200: printf("SDL_SENSORUPDATE\n"); 				break;
+		case 0x2000: printf("SDL_RENDER_TARGETS_RESET\n"); 		break;
+		case 0x2001: printf("SDL_RENDER_DEVICE_RESET\n"); 		break;
+		case 0x7F00: printf("SDL_POLLSENTINEL\n"); 				break;
+		case 0x8000: printf("SDL_USEREVENT\n"); 				break;
+		case 0xFFFF: printf("SDL_LASTEVENT\n"); 				break;
+		default: printf("wtf\n");
+	}
 }
